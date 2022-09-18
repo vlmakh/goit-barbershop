591694320339c5693c4ef161a0596b62e6f89663
vlmakh <vlmakh@gmail.com>
2022-09-18 10:04:39 +0300

    add header md-lg

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,12 +2,12 @@ const refs = {
   openModalBtn: document.querySelector("[data-menu-open]"),
   closeMobilBtn: document.querySelector("[data-menu-close]"),
   mobMenu: document.querySelector("[data-menu]"),
-  navMenu: document.querySelector("[data-nav]"),
+  // navMenu: document.querySelector("[data-nav]"),
 };
 
 refs.openModalBtn.addEventListener("click", toggleModal);
 refs.closeMobilBtn.addEventListener("click", toggleModal);
-refs.navMenu.addEventListener("click", toggleModal);
+// refs.navMenu.addEventListener("click", toggleModal);
 
 function toggleModal() {
   refs.mobMenu.classList.toggle("is-open");
